d140897437675c4ed25bddd1c947f21ee4bc7d96
Jianbin Liu <billyljb@gmail.com>
2024-11-07 13:47:22 +0800

    Update style.js

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -26,7 +26,7 @@ const Style = () => {
     
     #theme-hexo .menu-link:hover {
         background-size: 100% 2px;
-        color: #20bdf0;
+        color: #928CEE;
     }
 
     /* 设置了从上到下的渐变黑色 */
