ff1251cb83c377b037ad20432560c11781e3574f
Jianbin Liu <billyljb@gmail.com>
2024-12-17 22:23:25 +0800

    Update PostHero.js

--- themes/hexo/components/PostHero.js
+++ themes/hexo/components/PostHero.js
@@ -76,10 +76,11 @@ export default function PostHero({ post, siteInfo }) {
             </div>
 
             {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && (
-              <div className='busuanzi_container_page_pv font-light mr-2'>
-                <span className='mr-2 busuanzi_value_page_pv' />
-                {/* {locale.COMMON.VIEWS} */}
-              </div>
+              // <div className='busuanzi_container_page_pv font-light mr-2'>
+              //   <span className='mr-2 busuanzi_value_page_pv' />
+              //   {/* {locale.COMMON.VIEWS} */}
+                
+              // </div>
             )}
           </section>
 
