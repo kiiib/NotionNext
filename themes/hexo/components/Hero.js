14ebf28b81bbaf7bfa95993efa82ec96207f1060
Jianbin Liu <billyljb@gmail.com>
2025-07-16 15:28:06 +0800

    revert hidden big screen

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -56,44 +56,44 @@ const Hero = props => {
   }
 
   return (
-    // <header
-    //   id='header'
-    //   style={{ zIndex: 1 }}
-    //   className='w-full h-screen relative bg-black'>
-    //   <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
-    //     {/* 站点标题 */}
-    //     <div className='font-black text-4xl md:text-5xl shadow-text'>
-    //       {siteInfo?.title || siteConfig('TITLE')}
-    //     </div>
-    //     {/* 站点欢迎语 */}
-    //     <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
-    //       <span id='typed' />
-    //     </div>
+    <header
+      id='header'
+      style={{ zIndex: 1 }}
+      className='w-full h-screen relative bg-black'>
+      <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
+        {/* 站点标题 */}
+        <div className='font-black text-4xl md:text-5xl shadow-text'>
+          {siteInfo?.title || siteConfig('TITLE')}
+        </div>
+        {/* 站点欢迎语 */}
+        <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
+          <span id='typed' />
+        </div>
 
-    //     {/* 首页导航大按钮 */}
-    //     {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && (
-    //       <NavButtonGroup {...props} />
-    //     )}
+        {/* 首页导航大按钮 */}
+        {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && (
+          <NavButtonGroup {...props} />
+        )}
 
-    //     {/* 滚动按钮 */}
-    //     <div
-    //       onClick={scrollToWrapper}
-    //       className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white'>
-    //       <div className='opacity-70 animate-bounce text-xs'>
-    //         {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
-    //           locale.COMMON.START_READING}
-    //       </div>
-    //       <i className='opacity-70 animate-bounce fas fa-angle-down' />
-    //     </div>
-    //   </div>
+        {/* 滚动按钮 */}
+        <div
+          onClick={scrollToWrapper}
+          className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white'>
+          <div className='opacity-70 animate-bounce text-xs'>
+            {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
+              locale.COMMON.START_READING}
+          </div>
+          <i className='opacity-70 animate-bounce fas fa-angle-down' />
+        </div>
+      </div>
 
-    //   <LazyImage
-    //     id='header-cover'
-    //     alt={siteInfo?.title}
-    //     src={siteInfo?.pageCover}
-    //     className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
-    //   />
-    // </header>
+      <LazyImage
+        id='header-cover'
+        alt={siteInfo?.title}
+        src={siteInfo?.pageCover}
+        className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
+      />
+    </header>
   )
 }
 
